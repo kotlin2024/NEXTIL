607ae089937d81b769c5e194712af25569d5ac14
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2025-02-17 15:20:36 +0900

    Initial commit

---
+++ src/main/kotlin/hjp/nextil/NextilApplication.kt
@@ -0,0 +1,11 @@
+package hjp.nextil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NextilApplication
+
+fun main(args: Array<String>) {
+    runApplication<NextilApplication>(*args)
+}
